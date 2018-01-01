196538accf8fb2ce4cb3512c3c60ef07e2b4a0ab
Francesco Agnoletto <francesco.agnoletto92@gmail.com>
2018-01-01 12:08:45 +0100

    Refactor Home redux example

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,57 +1,39 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
 import * as actions from '../actions';
 
-class Home extends Component {
-  /**
-   * @param {*} props counterValue
-   */
-  constructor(props) {
-    super(props);
-
-    this.handleIncreaseValue = this.handleIncreaseValue.bind(this);
-    this.handleDecreaseValue = this.handleDecreaseValue.bind(this);
-  }
-  /**
-   * @return {number} this.props.counterValue +1
-   */
-  handleIncreaseValue() {
-    this.props.addToCounter();
-  }
-  /**
-   * @return {number} this.props.counterValue -1
-   */
-  handleDecreaseValue() {
-    this.props.removeFromCounter();
-  }
-
-  /**
-   * Main render of Homepage ideally all components
-   * used in this page should be children of this one
-   * @returns {*} our stateful page component
-   */
-  render() {
-    return (
-      <div>
-        <h2>Home Page</h2>
-        <p>The counter value is { this.props.counterValue }</p>
-        <button onClick={this.handleIncreaseValue}>Add</button>
-        <button onClick={this.handleDecreaseValue}>Remove</button>
-      </div>
-    );
-  }
-}
+const Home = ({ counterValue, handleIncreaseValue, handleDecreaseValue }) => (
+  <div>
+    <h2>Home Page</h2>
+    <p>The counter value is {counterValue }</p>
+    <button onClick={handleIncreaseValue}>Add</button>
+    <button onClick={handleDecreaseValue}>Remove</button>
+  </div>
+);
 
 Home.propTypes = {
   counterValue: PropTypes.number.isRequired,
-  addToCounter: PropTypes.func.isRequired,
-  removeFromCounter: PropTypes.func.isRequired,
+  handleIncreaseValue: PropTypes.func.isRequired,
+  handleDecreaseValue: PropTypes.func.isRequired,
 };
+
+// This is our redux wrapper component
+// it passes @value {int} counterValue
+// and the 2 actions to increase and decrease counterValue by 1
 
 const mapStateToProps = state => ({
   counterValue: state.counterReducer,
 });
 
-export default connect(mapStateToProps, actions)(Home);
+const mapDispatchToProps = dispatch => ({
+  handleIncreaseValue: () => {
+    dispatch(actions.addToCounter());
+  },
+  handleDecreaseValue: () => {
+    dispatch(actions.removeFromCounter());
+  },
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
